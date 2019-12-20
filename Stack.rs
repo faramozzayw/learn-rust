7229c404065d5af216b18d4afa4ad960b8ea9f55
faramozzayw <faramo.zayw@gmail.com>
2019-12-21 01:53:53 +0200

    The stack has been moved from the global scope to the module

--- Stack.rs
+++ Stack.rs
@@ -1,150 +1,129 @@
-use std::fmt;
 
-extern crate rand;
+//extern crate rand;
 
-use rand::Rng;
+//use rand::Rng;
 
-type Link = Option<Box<Node>>;
+mod fz {
+    use std::fmt;
 
-#[derive(Debug)]
-struct Node {
-    value: i32,
-    next: Link,
-}
+    type Link = Option<Box<Node>>;
 
-#[allow(dead_code)]
-impl Node {
-    fn new(value: i32, next: Link) -> Self {
-        Node { value, next }
-    }
-}
-
-#[derive(Debug)]
-struct Stack {
-    head: Link,
-}
-
-impl Stack {
-    fn new() -> Self {
-        Stack { head: None }
-    }
-}
-
-#[allow(dead_code)]
-impl Stack {
-    fn push(&mut self, value: i32) {
-        let new_node = Box::new(Node {
-            value,
-            next: self.head.take(),
-        });
-
-        self.head = Some(new_node);
+    #[derive(Debug)]
+    struct Node {
+        value: i32,
+        next: Link,
     }
 
-    fn pop(&mut self) -> Option<i32> {
-        self.head.take().map(|node| {
-            let value = node.value.clone();
-            self.head = node.next;
-            value
-        })
-    }
-
-    fn peek(&self) -> Option<&i32> {
-        self.head.as_ref().map(|node| &node.value)
-    }
-
-    fn empty(&self) -> bool {
-        match &self.head {
-            None => true,
-            _ => false,
+    #[allow(dead_code)]
+    impl Node {
+        fn new(value: i32, next: Link) -> Self {
+            Node { value, next }
         }
     }
 
-    fn to_vec(&self) -> Vec<i32> {
-        let mut vector: Vec<i32> = Vec::new();
-        let mut current_node = &self.head;
-
-        while let Some(node) = &current_node {
-            vector.push((&node.value).clone());
-            current_node = &node.next;
-        }
-
-        vector
+    #[derive(Debug)]
+    pub struct Stack {
+        head: Link,
     }
 
-   fn reverse(&mut self) {
-        let mut vector: Vec<i32> = Vec::new();
-
-        while let Some(node_value) = self.pop() {
-            vector.push(node_value);
+    impl Stack {
+        pub fn new() -> Self {
+            Stack { head: None }
         }
-        
-        vector.reverse();
-        
-        while let Some(elem) = vector.pop() {
-            self.push(elem);
+    }
+
+    #[allow(dead_code)]
+    impl Stack {
+        pub fn push(&mut self, value: i32) {
+            let new_node = Box::new(Node {
+                value,
+                next: self.head.take(),
+            });
+
+            self.head = Some(new_node);
+        }
+
+        pub fn pop(&mut self) -> Option<i32> {
+            self.head.take().map(|node| {
+                let value = node.value.clone();
+                self.head = node.next;
+                value
+            })
+        }
+
+        pub fn peek(&self) -> Option<&i32> {
+            self.head.as_ref().map(|node| &node.value)
+        }
+
+        pub fn empty(&self) -> bool {
+            match &self.head {
+                None => true,
+                _ => false,
+            }
+        }
+
+        pub fn to_vec(&self) -> Vec<i32> {
+            let mut vector: Vec<i32> = Vec::new();
+            let mut current_node = &self.head;
+
+            while let Some(node) = &current_node {
+                vector.push((&node.value).clone());
+                current_node = &node.next;
+            }
+
+            vector
+        }
+
+        pub fn reverse(&mut self) {
+            let mut vector: Vec<i32> = Vec::new();
+
+            while let Some(node_value) = self.pop() {
+                vector.push(node_value);
+            }
+
+            vector.reverse();
+
+            while let Some(elem) = vector.pop() {
+                self.push(elem);
+            }
+        }
+    }
+
+    impl fmt::Display for Stack {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let mut fstr = String::default();
+            let mut current_node = &self.head;
+
+            while let Some(node) = &current_node {
+                fstr += "|\t";
+                fstr += &node.value.to_string();
+                fstr += "\t|\n";
+                current_node = &node.next;
+            }
+
+            write!(f, "{}", fstr)
+        }
+    }
+
+    impl Drop for Stack {
+        fn drop(&mut self) {
+            let mut current_link = self.head.take();
+            while let Some(mut node) = current_link {
+                current_link = node.next.take();
+            }
         }
     }
 }
 
-impl fmt::Display for Stack {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut fstr = String::default();
-        let mut current_node = &self.head;
-
-        while let Some(node) = &current_node {
-            fstr += "|\t";
-            fstr += &node.value.to_string();
-            fstr += "\t|\n";
-            current_node = &node.next;
-        }
-        
-        write!(f, "{}", fstr)
-    }
-}
-
-impl Drop for Stack {
-    fn drop(&mut self) {
-        let mut current_link = self.head.take();
-        while let Some(mut node) = current_link {
-            current_link = node.next.take();
-        }
-    }
-}
-
-fn task(stack: &Stack) -> Stack {
-  let mut vect = stack.to_vec();
-  let len = vect.len() as i32;
-  let mut sum: i32 = 0;
-  
-  for item in &vect {
-    sum += item;
-  }
-  
-  let average = sum / len;
-  let mut new_stack = Stack::new();
-  
-  vect.reverse();
-  
-  for num in vect {
-    if num > average {
-      new_stack.push(num);
-    }
-  }
-  
-  new_stack
-}
+use crate::fz::Stack;
 
 fn main() {
-   let mut rng = rand::thread_rng();
-   let mut st = Stack::new();
-   
-   for n in 1 ..= 9 {
-     st.push(rng.gen_range(-50, 50));
-   }
-   
-   println!("Stack:\n{}", st);
-   
-   let nst = task(&st);
-   println!("New Stack:\n{}", nst);
+    //let mut rng = rand::thread_rng();
+    let mut st = Stack::new();
+
+    for n in 1..=9 {
+        st.push(n);
+    }
+
+    println!("Stack:\n{}", st);
 }
